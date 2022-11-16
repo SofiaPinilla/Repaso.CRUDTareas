00998287a4fdfcaf8fb307a5acb43770c43129b5
SofiaPinilla <contact@sofiapinilla.com>
2022-11-16 10:04:38 +0100

    Endpoints para traer todas las tareas y tarea por id

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -1,15 +1,35 @@
-const Task = require("../models/Task")
+const Task = require("../models/Task");
 
 const TaskController = {
-    async createTask (req, res){
-        try {
-            const task = await Task.create({...req.body, completed:false})
-            res.status(201).send({msg:"Task created", task})
-        } catch (error) {
-            console.error(error)
-            res.status(500).send({msg: "Error while creating task", error})
-        }
+  async createTask(req, res) {
+    try {
+      const task = await Task.create({ ...req.body, completed: false });
+      res.status(201).send({ msg: "Task created", task });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ msg: "Error while creating task", error });
     }
-}
+  },
 
-module.exports = TaskController
+  async getAllTask(req, res) {
+    try {
+      const tasks = await Task.find();
+      res.send(tasks);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ msg: "Error while getting tasks", error });
+    }
+  },
+
+  async getTaskById(req, res) {
+    try {
+      const task = await Task.findById(req.params._id);
+      res.send(task);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ msg: "Error while getting task", error });
+    }
+  },
+};
+
+module.exports = TaskController;
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,7 +1,9 @@
-const express = require("express")
-const TaskController = require("../controllers/TaskController")
-const router = express.Router()
+const express = require("express");
+const TaskController = require("../controllers/TaskController");
+const router = express.Router();
 
-router.post("/createTask", TaskController.createTask)
+router.post("/createTask", TaskController.createTask);
+router.get("/getAllTask", TaskController.getAllTask);
+router.get("/getTaskById/:_id", TaskController.getTaskById);
 
-module.exports = router
+module.exports = router;
